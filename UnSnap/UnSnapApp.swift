a5232635582f6aca30833aa5c94b81e2ee867f88
AvnishRana25 <avnishrana797@gmail.com>
2025-01-28 20:57:57 +0530

    Initial Commit

---
+++ UnSnap/UnSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnSnapApp.swift
+//  UnSnap
+//
+//  Created by Avnish Rana on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct UnSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
